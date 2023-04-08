033f0880e008cbe917c4699a586a412d405c79ad
Miguel <njobby_987@hotmail.com>
2023-04-08 19:09:56 -0300

    add endpoint that resolves product detail with quantity and subtotal

--- src/main/java/com/spiritualwarriors/ecommercebackend/controllers/ProductController.java
+++ src/main/java/com/spiritualwarriors/ecommercebackend/controllers/ProductController.java
@@ -5,8 +5,11 @@ import com.spiritualwarriors.ecommercebackend.services.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
+@CrossOrigin("*")
 public class ProductController {
 
     private final ProductService productService;
@@ -20,8 +23,14 @@ public class ProductController {
         return ResponseEntity.ok(productService.getAll());
     }
 
+    @GetMapping("/{details}")
+    public ResponseEntity<?> getDetailsResult(@PathVariable List<String> details) {
+        List<ProductService.ResultDTO> result = productService.getDetail(details);
+        return ResponseEntity.ok(result);
+    }
+
     @PostMapping()
-    public ResponseEntity<?> post(@RequestBody Product product) {
+    public ResponseEntity<?> add(@RequestBody Product product) {
         return ResponseEntity.ok(productService.add(product));
     }
 
--- src/main/java/com/spiritualwarriors/ecommercebackend/services/ProductService.java
+++ src/main/java/com/spiritualwarriors/ecommercebackend/services/ProductService.java
@@ -2,10 +2,15 @@ package com.spiritualwarriors.ecommercebackend.services;
 
 import com.spiritualwarriors.ecommercebackend.entities.Product;
 import com.spiritualwarriors.ecommercebackend.repositories.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+@Slf4j
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
@@ -15,15 +20,34 @@ public class ProductService {
     }
 
     public List<Product> getAll() {
+        log.info("GetAll called");
         return productRepository.findAll();
     }
 
     public Product add(Product product) {
+        log.info("add called");
         return productRepository.save(product);
     }
 
     public Boolean remove(Long id) {
+        log.info("remove called");
         productRepository.deleteById(id);
         return true;
     }
+
+    public List<ResultDTO> getDetail(List<String> details) {
+        log.info("getDetail called");
+        List<ResultDTO> result = new ArrayList<>();
+        details.forEach(detail -> {
+            String[] parts = detail.split("-");
+            Long id = Long.parseLong(parts[0]);
+            Integer quantity = Integer.parseInt(parts[1]);
+            Product productFound = productRepository.findById(id).orElseThrow(() -> new NoSuchElementException(""));
+            BigDecimal subtotal = productFound.getPrice().multiply(BigDecimal.valueOf(quantity));
+            result.add(new ResultDTO(productFound, quantity, subtotal));
+        });
+        return result;
+    }
+
+    public record ResultDTO(Product product, Integer quantity, BigDecimal subtotal){}
 }
